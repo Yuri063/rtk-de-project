e7b66a0db9d8996e892d4fe2fe311a63d35eadca
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-13 19:07:51 +0400

    Update dwh_project.py

--- dwh_project.py
+++ dwh_project.py
@@ -89,7 +89,7 @@ def get_jobs_context(phase_name, job):
 def get_jobs_context(phase_name, job):
     tasks = []
     search_path = os.path.join(ROOT_DIR, DATA_DIR, job.source_path)
-    for task_file_name in [i for i in os.listdir(search_path)) if i.endswith(job.mask)]:
+    for task_file_name in [i for i in os.listdir(search_path) if i.endswith(job.mask)]:
         tasks.append(PostgresOperator(
             task_id='{}_{}_{}'.format(phase_name, job.name, os.path.splitext(task_file_name)[0])),
             dag=dag,
