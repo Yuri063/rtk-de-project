15361ce6d28938a51a8298f89112de85884db6b6
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-14 00:15:25 +0400

    Update dds_link_traffic.sql

--- SQL/links/dds_link_traffic.sql
+++ SQL/links/dds_link_traffic.sql
@@ -5,7 +5,7 @@ with source_data as (
 		TRAFFIC_PK,
 		USER_PK, DEVICE_PK, IP_ADDR_PK,
 		LOAD_DATE, RECORD_SOURCE
-	from yfurman.project_view_traffic_one_year_{{ execution_date.year }}
+	from {{ params.prefix }}_view_traffic_one_year_{{ execution_date.year }}
 ),
 records_to_insert as (
 	select distinct 
@@ -13,11 +13,11 @@ records_to_insert as (
 		stg.USER_PK, stg.DEVICE_PK, stg.IP_ADDR_PK,
 		stg.LOAD_DATE, stg.RECORD_SOURCE
 	from source_data as stg 
-	left join yfurman.project_dds_link_traffic as tgt
+	left join {{ params.prefix }}_dds_link_traffic as tgt
 	on stg.TRAFFIC_PK = tgt.TRAFFIC_PK
 	where tgt.TRAFFIC_PK is null		
 )
-insert into yfurman.project_dds_link_traffic (
+insert into {{ params.prefix }}_dds_link_traffic (
 	TRAFFIC_PK,
 	USER_PK, DEVICE_PK, IP_ADDR_PK,
 	LOAD_DATE, RECORD_SOURCE)
