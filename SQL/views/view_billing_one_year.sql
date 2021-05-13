e0f34a0399f7f7961869d60ef6538b8e6a72bb0e
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-14 01:50:18 +0400

    Update view_billing_one_year.sql

--- SQL/views/view_billing_one_year.sql
+++ SQL/views/view_billing_one_year.sql
@@ -1,6 +1,6 @@
 -- 1.3.3 CREATE VIEW FOR BILLING
 
-create view {{ params.prefix }}_view_billing_one_year_{{ execution_date.year }} as (
+create or replace view {{ params.prefix }}_view_billing_one_year_{{ execution_date.year }} as (
 
 	with staging as (
 		with derived_columns as (
