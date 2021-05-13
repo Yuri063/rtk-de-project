f2c5ae8110ff066799092937b00405dc1fc653a1
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-14 00:05:16 +0400

    Update dds_hub_ip_addr.sql

--- SQL/hubs/dds_hub_ip_addr.sql
+++ SQL/hubs/dds_hub_ip_addr.sql
@@ -7,17 +7,17 @@ with row_rank_1 as (
 				partition by IP_ADDR_PK
 				order by LOAD_DATE ASC
 			) as row_num
-		from yfurman.project_view_traffic_one_year_{{ execution_date.year }}		
+		from {{ params.prefix }}_view_traffic_one_year_{{ execution_date.year }}		
 	) as h where row_num = 1
 ),
 records_to_insert as (
 		select a.IP_ADDR_PK, a.IP_ADDR_KEY, a.LOAD_DATE, a.RECORD_SOURCE
 		from row_rank_1 as a
-		left join yfurman.project_dds_hub_ip_addr as d
+		left join {{ params.prefix }}_dds_hub_ip_addr as d
 		on a.IP_ADDR_PK = d.IP_ADDR_PK
 		where d.IP_ADDR_PK is NULL
 )
-insert into yfurman.project_dds_hub_ip_addr (IP_ADDR_PK, IP_ADDR_KEY, LOAD_DATE, RECORD_SOURCE)
+insert into {{ params.prefix }}_dds_hub_ip_addr (IP_ADDR_PK, IP_ADDR_KEY, LOAD_DATE, RECORD_SOURCE)
 (
 	select IP_ADDR_PK, IP_ADDR_KEY, LOAD_DATE, RECORD_SOURCE
 	from records_to_insert
