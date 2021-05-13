9b5a0af8f7730693eac1edcc1801f2e98a094e8d
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-14 00:07:27 +0400

    Update dds_hub_pay_doc_type.sql

--- SQL/hubs/dds_hub_pay_doc_type.sql
+++ SQL/hubs/dds_hub_pay_doc_type.sql
@@ -7,17 +7,17 @@ with row_rank_1 as (
 					partition by PAY_DOC_TYPE_PK
 					order by LOAD_DATE ASC
 				) as row_num
-			from yfurman.project_view_payment_one_year_{{ execution_date.year }}  		
+			from {{ params.prefix }}_view_payment_one_year_{{ execution_date.year }}  		
 		) as h where row_num = 1
 	),	
 records_to_insert as (
 		select a.PAY_DOC_TYPE_PK, a.PAY_DOC_TYPE_KEY, a.LOAD_DATE, a.RECORD_SOURCE
 		from row_rank_1 as a
-		left join yfurman.project_dds_hub_pay_doc_type as d
+		left join {{ params.prefix }}_dds_hub_pay_doc_type as d
 		on a.PAY_DOC_TYPE_PK = d.PAY_DOC_TYPE_PK
 		where d.PAY_DOC_TYPE_PK is NULL
 )
-insert into yfurman.project_dds_hub_pay_doc_type (PAY_DOC_TYPE_PK, PAY_DOC_TYPE_KEY, LOAD_DATE, RECORD_SOURCE)
+insert into {{ params.prefix }}_dds_hub_pay_doc_type (PAY_DOC_TYPE_PK, PAY_DOC_TYPE_KEY, LOAD_DATE, RECORD_SOURCE)
 (
 	select PAY_DOC_TYPE_PK, PAY_DOC_TYPE_KEY, LOAD_DATE, RECORD_SOURCE
 	from records_to_insert
