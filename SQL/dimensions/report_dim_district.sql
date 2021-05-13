a071a238d8b7df26ea646a9e65f2c6386b15d5cd
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-13 23:54:40 +0400

    Update report_dim_district.sql

--- SQL/dimensions/report_dim_district.sql
+++ SQL/dimensions/report_dim_district.sql
@@ -1,7 +1,7 @@
 -- 10.3 INSERT INTO DEMENSION TABLEs REPORT_DIM_DISTRICT
 
-insert into yfurman.project_report_dim_district(district_key)
+insert into {{ params.prefix }}_report_dim_district(district_key)
 select distinct district as district_key 
-from yfurman.project_report_tmp_{{ execution_date.year }} a
-left join yfurman.project_report_dim_district b on b.district_key = a.district
+from {{ params.prefix }}_report_tmp_{{ execution_date.year }} a
+left join {{ params.prefix }}_report_dim_district b on b.district_key = a.district
 where b.district_key is null;
