d6df153f9af2d67b299555166b3d8cfb79b68d44
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-13 23:57:37 +0400

    Update report_dim_registration_year.sql

--- SQL/dimensions/report_dim_registration_year.sql
+++ SQL/dimensions/report_dim_registration_year.sql
@@ -1,7 +1,7 @@
 -- 10.4 INSERT INTO DEMENSION TABLEs REPORT_DIM_REGISTRATION_YEAR
 
-insert into yfurman.project_report_dim_registration_year(registration_year_key)
+insert into {{ params.prefix }}_report_dim_registration_year(registration_year_key)
 select distinct registration_year as registration_year_key 
-from yfurman.project_report_tmp_{{ execution_date.year }} a
-left join yfurman.project_report_dim_registration_year b on b.registration_year_key = a.registration_year
+from {{ params.prefix }}_report_tmp_{{ execution_date.year }} a
+left join {{ params.prefix }}_report_dim_registration_year b on b.registration_year_key = a.registration_year
 where b.registration_year_key is null;
