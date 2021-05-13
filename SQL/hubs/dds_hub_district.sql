f6910ad7a611b79be3eb331c5e7e8558614bf866
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-14 00:04:29 +0400

    Update dds_hub_district.sql

--- SQL/hubs/dds_hub_district.sql
+++ SQL/hubs/dds_hub_district.sql
@@ -7,17 +7,17 @@ with row_rank_1 as (
 				partition by DISTRICT_PK
 				order by LOAD_DATE ASC
 			) as row_num
-		from yfurman.project_view_mdm_one_year_{{ execution_date.year }}		
+		from {{ params.prefix }}_view_mdm_one_year_{{ execution_date.year }}		
 	) as h where row_num = 1
 ),
 records_to_insert as (
 		select a.DISTRICT_PK, a.DISTRICT_KEY, a.LOAD_DATE, a.RECORD_SOURCE
 		from row_rank_1 as a
-		left join yfurman.project_dds_hub_district as d
+		left join {{ params.prefix }}_dds_hub_district as d
 		on a.DISTRICT_PK = d.DISTRICT_PK
 		where d.DISTRICT_PK is NULL
 )
-insert into yfurman.project_dds_hub_district (DISTRICT_PK, DISTRICT_KEY, LOAD_DATE, RECORD_SOURCE)
+insert into {{ params.prefix }}_dds_hub_district (DISTRICT_PK, DISTRICT_KEY, LOAD_DATE, RECORD_SOURCE)
 (
 	select DISTRICT_PK, DISTRICT_KEY, LOAD_DATE, RECORD_SOURCE
 	from records_to_insert
