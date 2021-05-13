4a47a26e650cc19c85cede5a532045d5daa5a112
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-14 00:06:39 +0400

    Update dds_hub_legal_type.sql

--- SQL/hubs/dds_hub_legal_type.sql
+++ SQL/hubs/dds_hub_legal_type.sql
@@ -7,17 +7,17 @@ with row_rank_1 as (
 				partition by LEGAL_TYPE_PK
 				order by LOAD_DATE ASC
 			) as row_num
-		from yfurman.project_view_mdm_one_year_{{ execution_date.year }}		
+		from {{ params.prefix }}_view_mdm_one_year_{{ execution_date.year }}		
 	) as h where row_num = 1
 ),
 records_to_insert as (
 		select a.LEGAL_TYPE_PK, a.LEGAL_TYPE_KEY, a.LOAD_DATE, a.RECORD_SOURCE
 		from row_rank_1 as a
-		left join yfurman.project_dds_hub_legal_type as d
+		left join {{ params.prefix }}_dds_hub_legal_type as d
 		on a.LEGAL_TYPE_PK = d.LEGAL_TYPE_PK
 		where d.LEGAL_TYPE_PK is NULL
 )
-insert into yfurman.project_dds_hub_legal_type (LEGAL_TYPE_PK, LEGAL_TYPE_KEY, LOAD_DATE, RECORD_SOURCE)
+insert into {{ params.prefix }}_dds_hub_legal_type (LEGAL_TYPE_PK, LEGAL_TYPE_KEY, LOAD_DATE, RECORD_SOURCE)
 (
 	select LEGAL_TYPE_PK, LEGAL_TYPE_KEY, LOAD_DATE, RECORD_SOURCE
 	from records_to_insert
