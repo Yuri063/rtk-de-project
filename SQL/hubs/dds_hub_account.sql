bb69fd6042666a0fce2e03878817596ab6d7075e
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-14 00:00:51 +0400

    Update dds_hub_account.sql

--- SQL/hubs/dds_hub_account.sql
+++ SQL/hubs/dds_hub_account.sql
@@ -7,17 +7,17 @@ with row_rank_1 as (
 					partition by ACCOUNT_PK
 					order by LOAD_DATE ASC
 				) as row_num
-			from yfurman.project_view_payment_one_year_{{ execution_date.year }}  		
+			from {{ params.prefix }}_view_payment_one_year_{{ execution_date.year }}  		
 		) as h where row_num = 1
 	),	
 records_to_insert as (
 		select a.ACCOUNT_PK, a.ACCOUNT_KEY, a.LOAD_DATE, a.RECORD_SOURCE
 		from row_rank_1 as a
-		left join yfurman.project_dds_hub_account as d
+		left join {{ params.prefix }}_dds_hub_account as d
 		on a.ACCOUNT_PK = d.ACCOUNT_PK
 		where d.ACCOUNT_PK is NULL
 )
-insert into yfurman.project_dds_hub_account (ACCOUNT_PK, ACCOUNT_KEY, LOAD_DATE, RECORD_SOURCE)
+insert into {{ params.prefix }}_dds_hub_account (ACCOUNT_PK, ACCOUNT_KEY, LOAD_DATE, RECORD_SOURCE)
 (
 	select ACCOUNT_PK, ACCOUNT_KEY, LOAD_DATE, RECORD_SOURCE
 	from records_to_insert
