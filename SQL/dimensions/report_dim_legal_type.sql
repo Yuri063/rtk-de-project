ddecf83269a35ec9c1ca6fd6f4e1217dd2dffd0a
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-13 23:56:57 +0400

    Update report_dim_legal_type.sql

--- SQL/dimensions/report_dim_legal_type.sql
+++ SQL/dimensions/report_dim_legal_type.sql
@@ -1,7 +1,7 @@
 -- 10.2 INSERT INTO DEMENSION TABLEs REPORT_DIM_LEGAL_TYPE
 
-insert into yfurman.project_report_dim_legal_type(legal_type_key)
+insert into {{ params.prefix }}_report_dim_legal_type(legal_type_key)
 select distinct legal_type as legal_type_key 
-from yfurman.project_report_tmp_{{ execution_date.year }} a
-left join yfurman.project_report_dim_legal_type b on b.legal_type_key = a.legal_type
+from {{ params.prefix }}_report_tmp_{{ execution_date.year }} a
+left join {{ params.prefix }}_report_dim_legal_type b on b.legal_type_key = a.legal_type
 where b.legal_type_key is null;
