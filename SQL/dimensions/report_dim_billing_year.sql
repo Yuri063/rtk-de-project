bd0037c5d4f31d9b0299ea32207ec056804e73d5
Yuri063 <51289870+Yuri063@users.noreply.github.com>
2021-05-13 23:53:48 +0400

    Update report_dim_billing_year.sql

--- SQL/dimensions/report_dim_billing_year.sql
+++ SQL/dimensions/report_dim_billing_year.sql
@@ -1,7 +1,7 @@
 -- 10.1 INSERT INTO DEMENSION TABLEs REPORT_DIM_BILLING_YEAR
 
-insert into yfurman.project_report_dim_billing_year(billing_year_key)
+insert into {{ params.prefix }}_report_dim_billing_year(billing_year_key)
 select distinct billing_year as billing_year_key 
-from yfurman.project_report_tmp_{{ execution_date.year }} a
-left join yfurman.project_report_dim_billing_year b on b.billing_year_key = a.billing_year
+from {{ params.prefix }}_report_tmp_{{ execution_date.year }} a
+left join {{ params.prefix }}_report_dim_billing_year b on b.billing_year_key = a.billing_year
 where b.billing_year_key is null;
